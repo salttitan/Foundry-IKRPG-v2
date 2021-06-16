31c82fae503f97239c7d8cacd8aa417077e6f4b9
Fabio Vaz <fabiorvaz@gmail.com>
2021-06-16 09:41:13 -0300

    Created a character sheet. updated the css file to include styles for character sheet. updated the template.json to include fields for vital spiral control. included two new handlebars helpers (functions): -Times: a simple loop based in a number not a list of object -Ifcond: a if structure to compare two values by a condition.

--- boilerplate/iron-kingdoms-rpg/module/actor/actor.js
+++ boilerplate/iron-kingdoms-rpg/module/actor/actor.js
@@ -27,11 +27,6 @@ export class ikrpgActor extends Actor {
 
     // Make modifications to data here. For example:
 
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, ability] of Object.entries(data.abilities)) {
-      // Calculate the modifier using d20 rules.
-      ability.mod = Math.floor((ability.value - 10) / 2);
-    }
   }
 
 }
